eceb6147618a955be7aa132eb4d6074998739793
laoshuisheng <laoshuisheng@163.com>
2013-09-14 10:56:21 +0800

    test controller and logback.xml config commit

---
+++ springmvc-sample/src/main/java/com/lap/sapringmvc/controller/TestController.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2013 The JA-SIG Collaborative. All rights reserved.
+ * distributed with thi file and available online at
+ */
+package com.lap.sapringmvc.controller;
+
+/**
+ * @author 劳水生 
+ * @File TestController.java Date:2013-9-14 上午10:55:16 2013
+ */
+public class TestController {
+
+}
